6b80360da57cb89045d0216a278cc9018f7efbc1
Danilov_Nikolai <dan1k91@mail.ru>
2024-11-10 15:55:37 +0300

    components SortPopup & TopBar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 // components
-import { Container, Title, Categories } from '@/components/shared';
+import { Container, Title, TopBar } from '@/components/shared';
 
 export default function Home() {
   return (
     <>
       <Container className="mt-10">
         <Title text="Все пиццы" size="lg" className="font-extrabold" />
-
-        <Categories />
+        <TopBar />
       </Container>
     </>
   );
