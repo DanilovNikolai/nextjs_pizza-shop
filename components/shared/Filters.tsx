0490b6234e4c737d61b09375a8e2e8d1024674ab
Danilov_Nikolai <dan1k91@mail.ru>
2024-11-13 19:54:05 +0300

    component CheckboxFiltersGroup upd

--- components/shared/Filters.tsx
+++ components/shared/Filters.tsx
@@ -57,6 +57,12 @@ export const Filters: React.FC<FiltersProps> = ({ className }) => {
           { text: 'Солёные огурчики', value: '4' },
           { text: 'Красный лук', value: '5' },
           { text: 'Томаты', value: '6' },
+          { text: 'Сырный соус', value: '1' },
+          { text: 'Моцарелла', value: '2' },
+          { text: 'Чеснок', value: '3' },
+          { text: 'Солёные огурчики', value: '4' },
+          { text: 'Красный лук', value: '5' },
+          { text: 'Томаты', value: '6' },
         ]}
       />
     </div>
