5a5b1e95e1708454f0491d87730ad7b02e66e78b
Danilov_Nikolai <dan1k91@mail.ru>
2024-11-26 22:49:27 +0300

    ProductImage component added

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,9 @@
-import React from 'react';
+// prisma
+import { prisma } from '@/prisma/prisma-client';
+// container
+import { Container, ProductImage } from '@/components/shared';
+// next
+import { notFound } from 'next/navigation';
 
 interface ProductPageProps {
   params: {
@@ -6,8 +11,20 @@ interface ProductPageProps {
   };
 }
 
-const ProductPage: React.FC<ProductPageProps> = ({ params: { id } }) => {
-  return <div>ProductPage {id}</div>;
-};
+export default async function ProductPage({ params: { id } }: ProductPageProps) {
+  const product = await prisma.product.findFirst({
+    where: {
+      id: Number(id),
+    },
+  });
 
-export default ProductPage;
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+      <ProductImage imageUrl={product.imageUrl} size={20}/>
+    </Container>
+  );
+}
