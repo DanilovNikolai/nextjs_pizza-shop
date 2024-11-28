95f42cab459127dda54f148bd48da6ea5543b793
Danilov_Nikolai <dan1k91@mail.ru>
2024-11-28 20:08:26 +0300

    ItemVariantsSelector components added

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,7 @@
 // prisma
 import { prisma } from '@/prisma/prisma-client';
 // container
-import { Container, ProductImage } from '@/components/shared';
+import { Container, ProductImage, Title } from '@/components/shared';
 // next
 import { notFound } from 'next/navigation';
 
@@ -24,7 +24,17 @@ export default async function ProductPage({ params: { id } }: ProductPageProps) 
 
   return (
     <Container className="flex flex-col my-10">
-      <ProductImage imageUrl={product.imageUrl} size={20}/>
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} size={30} />
+
+        <div className="w-[490-px] bg-[#f8f8f8] p-7">
+          <Title text={product.name} size="md" className="font-extrabold mb-1" />
+
+          <p className="text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, porro?
+          </p>
+        </div>
+      </div>
     </Container>
   );
 }
