617e43349ba8f88722a7440746337e5038d4b9c5
Danilov_Nikolai <dan1k91@mail.ru>
2024-12-11 23:59:01 +0300

    cart components conditional rendering upd

--- shared/components/CartDrawer.tsx
+++ shared/components/CartDrawer.tsx
@@ -52,69 +52,75 @@ export const CartDrawer: React.FC<PropsWithChildren<Props>> = ({ children }) => 
 
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
         <SheetTitle className="hidden" />
-        <SheetHeader>
-          {items.length === 1 && (
-            <SheetTitle>
-              В корзине <span className="font-bold">{items.length} товар</span>
-            </SheetTitle>
-          )}
-          {items.length > 1 && items.length < 5 && (
-            <SheetTitle>
-              В корзине <span className="font-bold">{items.length} товара</span>
-            </SheetTitle>
-          )}
-          {(items.length === 0 || items.length >= 5) && (
-            <SheetTitle>
-              В корзине <span className="font-bold">{items.length} товаров</span>
-            </SheetTitle>
-          )}
-        </SheetHeader>
+        {totalAmount > 0 && (
+          <SheetHeader>
+            {items.length === 1 && (
+              <SheetTitle>
+                В корзине <span className="font-bold">{items.length} товар</span>
+              </SheetTitle>
+            )}
+            {items.length > 1 && items.length < 5 && (
+              <SheetTitle>
+                В корзине <span className="font-bold">{items.length} товара</span>
+              </SheetTitle>
+            )}
+            {items.length >= 5 && (
+              <SheetTitle>
+                В корзине <span className="font-bold">{items.length} товаров</span>
+              </SheetTitle>
+            )}
+          </SheetHeader>
+        )}
 
         <SheetDescription className="hidden" />
 
-        <div className="-mx-6 mt-5 overflow-auto scrollbar flex-1">
-          {items.map((item) => (
-            <div className="mb-2" key={item.id}>
-              <CartDrawerItem
-                id={item.id}
-                name={item.name}
-                imageUrl={item.imageUrl}
-                price={item.price}
-                quantity={item.quantity}
-                disabled={item.disabled}
-                onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
-                onClickRemove={() => removeCartItem(item.id)}
-                details={
-                  item.pizzaSize && item.pizzaType
-                    ? getCartItemDetails(
-                        item.ingredients,
-                        item.pizzaType as PizzaType,
-                        item.pizzaSize as PizzaSize
-                      )
-                    : ''
-                }
-              />
+        {totalAmount > 0 && (
+          <>
+            <div className="-mx-6 mt-5 overflow-auto scrollbar flex-1">
+              {items.map((item) => (
+                <div className="mb-2" key={item.id}>
+                  <CartDrawerItem
+                    id={item.id}
+                    name={item.name}
+                    imageUrl={item.imageUrl}
+                    price={item.price}
+                    quantity={item.quantity}
+                    disabled={item.disabled}
+                    onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
+                    onClickRemove={() => removeCartItem(item.id)}
+                    details={
+                      item.pizzaSize && item.pizzaType
+                        ? getCartItemDetails(
+                            item.ingredients,
+                            item.pizzaType as PizzaType,
+                            item.pizzaSize as PizzaSize
+                          )
+                        : ''
+                    }
+                  />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
 
-        <SheetFooter className="-mx-6 bg-white p-8">
-          <div className="w-full">
-            <div className="flex mb-4">
-              <span className="flex flex-1 text-lg text-neutral-500">
-                Итого
-                <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
-              </span>
-              <span className="font-bold text-lg">{totalAmount} ₽</span>
-            </div>
-            <Link href="/cart">
-              <Button type="submit" className="w-full h-12 text-base">
-                Оформить заказ
-                <ArrowRight className="w-5 ml-2" />
-              </Button>
-            </Link>
-          </div>
-        </SheetFooter>
+            <SheetFooter className="-mx-6 bg-white p-8">
+              <div className="w-full">
+                <div className="flex mb-4">
+                  <span className="flex flex-1 text-lg text-neutral-500">
+                    Итого
+                    <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
+                  </span>
+                  <span className="font-bold text-lg">{totalAmount} ₽</span>
+                </div>
+                <Link href="/cart">
+                  <Button type="submit" className="w-full h-12 text-base">
+                    Оформить заказ
+                    <ArrowRight className="w-5 ml-2" />
+                  </Button>
+                </Link>
+              </div>
+            </SheetFooter>
+          </>
+        )}
       </SheetContent>
     </Sheet>
   );
