55a14114cb52451b138a1a51c9638e20f8b97fca
Danilov_Nikolai <dan1k91@mail.ru>
2024-12-12 21:48:23 +0300

    CheckoutItemDetails component added

---
+++ shared/components/CheckoutItemDetails.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+// cn
+import { cn } from '../lib/utils';
+
+interface Props {
+  title?: ReactNode;
+  value?: string;
+  className?: string;
+}
+
+export const CheckoutItemDetails: React.FC<Props> = ({ title, value, className }) => {
+  return (
+    <div className={cn('flex my-4', className)}>
+      <div className="flex flex-1 text-lg text-neutral-500">
+        {title}
+        <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2"></div>
+      </div>
+
+      <span className="font-bold text-lg">{value}</span>
+    </div>
+  );
+};
