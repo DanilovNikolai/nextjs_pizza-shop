770912e447d98be03cbf435c3ad5b47380e85aa1
Danilov_Nikolai <dan1k91@mail.ru>
2024-12-29 23:59:35 +0300

    auth via google added

--- shared/constants/auth-options.ts
+++ shared/constants/auth-options.ts
@@ -1,6 +1,7 @@
 // next-auth
 import { AuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
 // prisma
 import { prisma } from '@/prisma/prisma-client';
@@ -22,6 +23,11 @@ export const authOptions: AuthOptions = {
           role: 'USER' as UserRole,
         };
       },
+    }),
+
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
     }),
 
     CredentialsProvider({
