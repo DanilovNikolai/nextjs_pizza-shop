f047c98d50e65dfa9be19d5a16cc3f69df43aca6
Danilov_Nikolai <dan1k91@mail.ru>
2024-12-31 18:04:24 +0300

    /auth/verify server api route fixed - 'force-dynamic' added

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -1,6 +1,8 @@
 import { prisma } from '@/prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest) {
   try {
     // Сохраняем код при переходе по ссылке из письма - /auth/verify?code=${code}
