3401bbedc7750739e1861475a2c96366081bad88
Danilov_Nikolai <dan1k91@mail.ru>
2025-01-25 22:32:14 +0300

    not-auth page screen responsive added

--- app/(root)/not-auth/page.tsx
+++ app/(root)/not-auth/page.tsx
@@ -2,7 +2,7 @@ import { InfoBlock } from '@/shared/components';
 
 export default function NotAuthPage() {
   return (
-    <div className="flex flex-col items-center justify-center mt-40">
+    <div className="flex flex-col items-center justify-center mt-40 mmd:mt-5">
       <InfoBlock
         title="Доступ запрещён"
         text="Данную страницу могут просматривать только авторизованные пользователи"
--- shared/components/InfoBlock.tsx
+++ shared/components/InfoBlock.tsx
@@ -18,9 +18,14 @@ interface Props {
 
 export const InfoBlock: React.FC<Props> = ({ className, title, text, imageUrl }) => {
   return (
-    <div className={cn(className, 'flex items-center justify-between w-[840px] gap-12')}>
+    <div
+      className={cn(
+        className,
+        'flex items-center justify-between w-max-[840px] px-10 gap-12 mmd:flex-col-reverse mmd:px-7 mmd:gap-10'
+      )}
+    >
       <div className="flex flex-col">
-        <div className="w-[445px]">
+        <div className="w-[445px] mmd:w-[90%]">
           <Title size="lg" text={title} className="font-extrabold" />
           <p className="text-gray-400 text-lg">{text}</p>
         </div>
@@ -40,7 +45,7 @@ export const InfoBlock: React.FC<Props> = ({ className, title, text, imageUrl })
         </div>
       </div>
 
-      <img src={imageUrl} alt={title} width={300} />
+      <img src={imageUrl} alt={title} width={300} className="mmd:w-[50%]" />
     </div>
   );
 };
