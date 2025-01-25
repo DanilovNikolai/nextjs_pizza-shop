4e9b198c69decad8829cb0e72cccf720610d33d6
Danilov_Nikolai <dan1k91@mail.ru>
2025-01-25 21:44:34 +0300

    email redirect link fixed

--- shared/components/email-templates/VerificationUserEmail.tsx
+++ shared/components/email-templates/VerificationUserEmail.tsx
@@ -6,16 +6,16 @@ interface Props {
 export const VerificationUserEmail: React.FC<Props> = ({ code, fullName }) => {
   return (
     <div>
-      <h1>
-        Здравствуйте, <b>{fullName}</b>!
-      </h1>
+      <h1>Здравствуйте, {fullName}!</h1>
       <p>Спасибо за регистрацию на нашем сайте!</p>
       <p>
         Ваш код подтверждения: <h2>{code}</h2>
       </p>
 
       <p>
-        <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>Подтвердить регистрацию</a>
+        <a href={`https://nextjs-pizza-shop.vercel.app/api/auth/verify?code=${code}`}>
+          Подтвердить регистрацию
+        </a>
       </p>
     </div>
   );
